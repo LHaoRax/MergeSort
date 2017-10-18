01fa3aebaa9b85076a6ecf2ce49ff826a30eb47a
LHaoRax <32895002+LHaoRax@users.noreply.github.com>
2017-10-18 21:49:08 +0800

    Update mergesort.cpp
    增加自然归并排序

--- mergesort.cpp
+++ mergesort.cpp
@@ -7,6 +7,7 @@ class Merge_Sort
 {
 	int *data;
 	int count;
+	int *Array;
 public:
 	Merge_Sort(int N);
 	void insert_sort(int low, int high);
@@ -18,16 +19,22 @@ public:
 	void mergesort_btt(int *temp);//自底向上的归并排序
 	void Mergesort_btt();
 
+	int Pass();
+	void natural_merge_sort(int *temp);
+	void natural_Mergesort();
 
 	void swap(int i, int j);
 
 	void issorted();
+
 	void show();
+	void Ashow();
 };
 Merge_Sort::Merge_Sort(int N)
 {
 	count = N;
 	data = new int[N];
+	Array = new int[N];
 	for (int i = 0; i < N; i++)
 	{
 		data[i] = (rand() % (100 - 0))+ 0;
@@ -57,6 +64,14 @@ void Merge_Sort::show()
 	for (int i = 0; i < count; i++)
 	{
 		cout<<data[i]<<" ";
+	}
+	cout<<endl;
+}
+void Merge_Sort::Ashow()
+{
+	for (int i = 0; i < count; i++)
+	{
+		cout<<Array[i]<<" ";
 	}
 	cout<<endl;
 }
@@ -138,11 +153,59 @@ void Merge_Sort::insert_sort(int low, int high)
 		data[j] = temp;
 	}
 }
+
+int Merge_Sort::Pass ()  
+{
+    int num = 0;
+    int biger = data[0] ;
+    Array[num++] = 0 ;  // 将全局数组下标为0的值记为0
+
+    for (int i = 1 ; i < count ; i++ ) 
+	{
+        if ( data[i] >= biger ) 
+		{
+            biger = data[i] ;
+        } 
+		else
+		{
+            Array[num++] = i ;
+            biger = data[i] ;
+        }
+    }
+
+    // array数组存储每一组有序的数组的起始元素的下标,同时存储最后一个元素的下标+1,为了方便最后一组元素的合并二需要!!!
+    Array[num++] = count ;
+
+    return num ;        // num此时为数组最后一个元素下标+1
+}
+void Merge_Sort::natural_merge_sort (int *temp) 
+{
+    int num = Pass() ;
+    while ( num != 2 ) 
+	{
+        // num = 2 说明已经排序完成即只存储了下标为1的元素
+        // 每循环一次,执行一次pass函数
+
+        for (int i = 0 ; i < num ; i += 2 ) 
+		{
+        // array[i]即合并数据的起点; array[i+2]后一组的后面一组的起点-1即合并数据的终点; array[i+1]后一组的起点-1即合并数据的中点
+            merge(Array[i], Array[i+1] - 1, Array[i+2] - 1, temp) ;
+        }
+    num = Pass() ;
+    }
+}
+void Merge_Sort::natural_Mergesort()
+{
+	int *temp = new int[count];
+	natural_merge_sort(temp);
+	delete temp;
+}
+
 void main()
 {
 	Merge_Sort s1(10);
 	s1.show();
-	s1.Mergesort();
+	s1.natural_Mergesort();
 	s1.show();
 	s1.issorted();
 	system("pause");
